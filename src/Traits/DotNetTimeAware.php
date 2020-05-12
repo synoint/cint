61737c66a69099191173f5d26252724ee1e6ad6b
Mindaugas Liubinas <mli@synoint.com>
2020-05-12 14:37:01 +0300

    Deprecate and replace time functions

--- src/Traits/DotNetTimeAware.php
+++ src/Traits/DotNetTimeAware.php
@@ -6,6 +6,8 @@ trait DotNetTimeAware
     /**
      * @param int $ticks
      *
+     * @deprecated - use cintTimestampToUnixTimestamp
+     *
      * @return int
      */
     protected function ticksToTime($ticks): int {
@@ -14,6 +16,8 @@ trait DotNetTimeAware
 
     /**
      * @param int $ticks
+     *
+     * @deprecated - use cintTimestampToDatetime
      *
      * @return string
      */
@@ -25,10 +29,42 @@ trait DotNetTimeAware
     /**
      * @param int $ticks
      *
+     * @deprecated - use cintTimestampToDate
+     *
      * @return string
      */
     protected function ticksToDate(int $ticks): string
     {
         return date('Y-m-d', (floor($ticks / 1000) - 61903));
+    }
+
+    /**
+     * @param int $ts
+     *
+     * @return int
+     */
+    protected function cintTimestampToUnixTimestamp(int $ts)
+    {
+        return (int) floor($ts / 1000);
+    }
+
+    /**
+     * @param int ts
+     *
+     * @return string
+     */
+    protected function cintTimestampToDatetime(int $ts): string
+    {
+        return date('Y-m-d H:i:s', floor($ts / 1000));
+    }
+
+    /**
+     * @param int ts
+     *
+     * @return string
+     */
+    protected function cintTimestampToDate(int $ts): string
+    {
+        return date('Y-m-d', floor($ts / 1000));
     }
 }
