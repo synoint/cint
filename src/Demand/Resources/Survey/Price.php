1f024805461edc32ff402bf6fda031e83d636812
gediminas <gka@synoint.com>
2020-07-31 13:18:59 +0300

    Update price estimation function, to accept separate variables instead of array

--- src/Demand/Resources/Survey/Price.php
+++ src/Demand/Resources/Survey/Price.php
@@ -24,13 +24,15 @@ class Price
     }
 
     /**
-     * @param array $params - required(incidenceRate, lengthOfInterview, countryId)
+     * @param int $incidenceRate
+     * @param int $lengthOfInterview
+     * @param int $countryId
      * Returns survey price estimation
      *
      * @return array
      */
-    public function getEstimation($params): array
+    public function getEstimation(int $incidenceRate, int $lengthOfInterview, int $countryId): array
     {
-        return $this->client->post('/ordering/quotes/', $params);
+        return $this->client->post('/ordering/quotes/', ['incidenceRate' => $incidenceRate, 'lengthOfInterview' => $lengthOfInterview, 'countryId' => $countryId]);
     }
 }
