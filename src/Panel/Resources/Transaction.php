00b8ff51466cbbd6038e382f66a8e42aafeb9802
zygimantas <zba@synoint.com>
2021-06-01 14:13:33 +0300

    Added panelist update, unsubscribe and insert transactions methods.

--- src/Panel/Resources/Transaction.php
+++ src/Panel/Resources/Transaction.php
@@ -1,6 +1,8 @@
 <?php
 namespace Syno\Cint\Panel\Resources;
 
+use GuzzleHttp\Exception\ClientException;
+use Symfony\Component\HttpFoundation\Response;
 use Syno\Cint\Panel\Client;
 use Syno\Cint\Traits\StatusCodes;
 
@@ -33,6 +35,43 @@ class Transaction
         }
 
         return $result;
+    }
+
+    public function insertTransaction(string $apiKey, string $apiSecret, int $panelistId, int $points): array
+    {
+        $identifier = 'customTransaction-' . (new \DateTime('now'))->format('Y-m-d H:i:s');
+        try {
+            $response = $this->client->post(
+                sprintf('%s/panels/%s/panelists/%d/transactions', $this->client->getApiUrl(), $apiKey, $panelistId),
+                [
+                    'auth'    => [$apiKey, $apiSecret],
+                    'headers' => ['Accept' => 'application/json'],
+                    'json'    => [
+                        'transaction' => [
+                            'type'       => 'tt_rew',
+                            'points'     => $points,
+                            'identifier' => $identifier
+                        ],
+                    ]
+                ]
+            );
+            if (Response::HTTP_CREATED === $response->getStatusCode()) {
+                $result = json_decode($response->getBody());
+
+                return $result->transaction;
+            }
+        } catch (ClientException $e) {
+            $summary = [];
+            $errors  = json_decode($e->getResponse()->getBody()->getContents(), true);
+
+            foreach ($errors as $field => $messages) {
+                $summary[] = sprintf('%s %s', $field, implode('; ', $messages));
+            }
+
+            throw new \RuntimeException(implode('; ', $summary), $e->getCode());
+        }
+
+        return [];
     }
 }
 
